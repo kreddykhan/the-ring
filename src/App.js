b5f48fd7ec02a2ba5f5508bb1738ac7e2ea26b2f
admin <admin@admins-MacBook-Pro.local>
2022-10-09 23:20:59 -0700

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
+import React from 'react';
+import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Notes from './pages/Notes';
+import Timer from './pages/Timer';
+import Videos from './pages/Videos';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <div className="App">
-        Hello World
-      </div>
-    </div>
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/notes' element={<Notes/>} />
+        <Route path='/timer' element={<Timer/>} />
+        <Route path='/videos' element={<Videos/>} />
+      </Routes>
+    </Router>
   );
 }
+  
 
 export default App;
+
