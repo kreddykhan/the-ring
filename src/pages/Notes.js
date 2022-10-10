55cc0f1b15acd46b8455de718eee9de1b772d7fb
admin <admin@admins-MacBook-Pro.local>
2022-10-10 11:05:19 -0700

    Writing up notes

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -2,8 +2,136 @@ import '../App.css';
 
 function Notes() {
   return (
-    <div className="App">
-      Notes
+    <div className="App">      
+      <div className="Note">
+        Relax <br/>
+        <ul>
+          <li>Relax at all times, but especially when punching.</li>
+          <li>Breath out with every punch.</li>
+          <li>Conserve your energy.</li>
+        </ul>
+      </div>
+
+      <div className="Note">
+        Stance/Body <br/>
+        <ul>
+          <li>Weight
+            <ul>
+              <li>Weight should mostly be on your back foot. Distribute weight to be roughly 65% on your back foot and 35% on your front foot.</li>
+              <li>Keep weight on the balls of feet. Your heels shouldn't have any weight on them.</li>
+              <li>Weight distribution is always changing as you perform different punches.</li>
+            </ul>
+          </li>
+          <li>Legs/Feet
+            <ul>
+              <li>Being bottom heavy ie weight on both legs is absolutely imperative!</li>
+              <li>Hips and legs are your source of power.</li>
+              <li>Legs are your foundation. Keep them set on the ground roughly shoulder width apart when punching.</li>
+              <li>Keep your feet shoulder width length apart.</li>
+              <li>ALWAYS keep both feet on the ground.</li>
+              <li>Stay on the balls of feet.</li>
+              <li>If your head goes past your knee you are out of position and in jeopardy of being caught. Also you will be wasting energy getting back into position.</li>
+            </ul>
+          </li>
+          <li>Chin
+            <ul>
+              <li>Always keep your chin DOWN.</li>
+              <li>Bury your chin behind your lead hand shoulder. That is to say tuck your chin into the shoulder of your jab hand ie left shoulder for orthodox, R for southpaw.</li>
+            </ul>
+          </li>
+          <li>General
+            <ul>
+              <li>Stay behind front knee.</li>
+              <li>Always try to be in position, particularly to fire a jab.</li>
+              <li>Learn to have your hands in different positions for different moves. Everything has a purpose.</li>
+              <li>Keep yourself at angle. Don't square up to your opponent, but also avoid crossing feet.</li>
+              <li>Keep elbows tucked in.</li>
+              <li>Rotate hands on impact.</li>
+              <li>Your position will dictate who is in control.</li>
+              <li>Do crunches. The only way to increase ability to take body shots is working your abs!</li>
+            </ul>
+          </li>
+        </ul>
+        
+      </div>
+
+      <div className="Note">
+        Movement <br/>
+        <ul>
+          <li>Move! Never stand still unless you're not punching or moving your head.</li>
+          <li>Your hands, head or feet MUST always be moving. If not it better be between rounds!</li>
+          <li>NEVER stand still or be a stationary target for your opponent.</li>
+          <li>When moving, first move the foot that is closest to the direction you're going and then follow with the same distance with your other foot. This prevents you from crossing your feet.</li>
+          <li>Stay on the balls of your feet.</li>
+          <li>Use your front foot to get in and your back foot to step back out.</li>
+          <li>Don't be in range to be hit unless you're punching or setting something up.</li>
+          <li>Never freeze after punching and keep standing in front of your opponent. Step left or right.</li>
+          <li>Avoid backing up straight.</li>
+          <li>You should try to use feet to control range, distance so YOU decide when to punch and when to rest.</li>
+        </ul>
+      </div>
+
+      <div className="Note">
+        Offense
+        <ul>
+          <li>General Offense
+            <ul>
+              <li>Start and finish all you do with the jab</li>
+              <li>Always be in position to fire the jab.</li>
+              <li>When in doubt fire some hard and fast lead hand jabs in 2s, 3s even 4s</li>
+              <li>Keep opponents guessing; feint, throw jabs in 1s, 2s and 3s.</li>
+              <li>Don't repeat the same thing or do things in patterns unless you're setting a trap. Change up your sequence of jabs in 2s and 3s and then cross. But always lead with the jab.</li>
+              <li>Feints can be done with your hands or foot work or both.</li>
+              <li>Get your opponent to react. Make steps with your front foot to get a reaction, then fire explosive jabs or 1-2.</li>
+              <li>Think before throwing anything except for the jab</li>
+              <li>The jab will get you back in position while at the same time having opponent have to deal with</li>
+              <li>Keep punches short</li>
+            </ul>
+          </li>
+          <li>Jab
+            <ul>
+              <li>When in doubt JAB!</li>
+              <li>Jab can be thrown any time. Everything should start and finish with the jab.</li>
+              <li>Jab to the body or head. Adjust your jab and body as needed.
+                <ul>
+                  <li>Crouch while using the jab or dip your body to line up with opponents body.</li>
+                  <li>Throw different variations of jab ie up jab, shoulder roll jab, cross arm jab or sideways jab. </li>
+                </ul> 
+              </li>
+              <li>Use your jab before and after everything you do and in between</li>
+              <li>Jab off the back foot. Push off back foot to get leverage.</li>
+            </ul>
+          </li>
+          <li>Left Hook
+            <ul>
+              <li>Hand must stay above elbows.</li>
+              <li>Keep elbow in until impact.</li>
+              <li>Think of punching with your hip.</li>
+              <li>Use your legs/feet to step in when you need to close the distance with hook.</li>
+              <li>Hip and leg rotation are never seperate from the punches.</li>
+              <li>Left hand should remain in straight.</li>              
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <div className="Note">
+        Defense <br/>
+        <ul>
+          <li>ALWAYS keep your eyes on your opponent.</li>
+          <li>Find a spot in the middle of their chest or right below their neck to look at.</li>
+          <li>Hand that isn't punching should always be used as defensive tool. Keep it pinned to your cheek or palm out in front of your face</li>
+          <li>NEVER stand still or be a stationary target.</li>
+          <li>Make opponent struggle to be lined up to face you. This way they'll struggle to get in position to land or get punches off.</li>
+          <li>Punch between their punches. This takes timing and practive.</li>
+          <li>Move your head.</li>
+          <li>Your eyes are important for defense. Ideally if you can see it you shouldn't be hit. Even if you see right when it's landing you can avoid taking the full force of it.</li>
+          <li>If you think you may see whatever you're doing from opponents perspective make necessary adjustments to make it undetectable.</li>
+          <li>If you double and triple your jab with your chin tucked behind your shoulder you'll catch their counter every time.</li>
+          <li>Breath, relax step out and regroup.</li>
+          <li>Try to show nothing ie fatigue.</li>
+        </ul>        
+      </div>
     </div>
   );
 }
