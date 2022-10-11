16189009e67df7b2c427d23c7d4101b2e2b36772
admin <admin@admins-MacBook-Pro.local>
2022-10-10 18:43:44 -0700

    Setting up Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,10 @@ import '../App.css';
 
 function Home() {
   return (
-    <div className="App">
-      Home
+    <div className="Home">
+      <div className="title">Don't take your fists to the bag, <br/>let the bag come to your fists<br/></div>
+      <br/><br/>
+      A website for Martin O'Malley's suggested notes and videos and a built in boxing timer.
     </div>
   );
 }
